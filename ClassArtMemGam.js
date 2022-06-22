a69f849ca784aedf2e6851383d94b39f67a7b939
Aleksandra Galach <rachelkakoderka@gmail.com>
2022-06-22 22:37:25 +0200

    added eventlisteners onclick

--- ClassArtMemGam.js
+++ ClassArtMemGam.js
@@ -1,6 +1,31 @@
-document.querySelector('#boardgame').addEventListener('click',(e)=> {
+// even delegation using bubbling to minimize amoount of eventListerners
+document.querySelector("#boardgame").addEventListener("click", game);
+
+const gameState = {
+	secondCardFlipped: false,
+	turns: 0,
+	matches: 0,
+};
+
+function game(e) {
 	let cardID = e.target.id;
 	let elem = document.querySelector(`#${cardID}`);
-	return elem.classList.add("rewers");
-	
-})
+	gameState.turns++;
+
+	console.log(gameState.secondCardFlipped);
+	//is clicked card first or second card flipped?
+	if (gameState.secondCardFlipped) {
+		//pierwsza odwrócona
+		elem.classList.add("flipped");
+		gameState.secondCardFlipped = false;
+		setTimeout(() => {
+			let toBeREwersed = document.querySelectorAll(".flipped");
+		}, 1000);
+	} else {
+		elem.classList.add("flipped");
+		gameState.secondCardFlipped = true;
+		//druga odwrócona
+	}
+	console.log(gameState.secondCardFlipped);
+	console.log(gameState.turns);
+}
